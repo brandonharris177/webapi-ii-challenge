724b752e4937b658fe8d3bb976ad4bda530894fe
Brandon-Harris <brandonharris177@gmail.com>
2019-10-01 14:22:02 -0600

    get by id working

--- Router/Router.js
+++ Router/Router.js
@@ -14,4 +14,36 @@ router.get('/', (req, res) => {
     })
 });
 
+router.get('/', (req, res) => {
+    Hubs.find(req.query)
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: 'Error retrieving the hubs',
+      });
+    });
+  });
+  
+router.get('/:id', (req, res) => {
+data.findById(req.params.id)
+.then(post => {
+    if (post) {
+    res.status(200).json(post);
+    } else {
+    res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+})
+.catch(error => {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+    message: 'Error retrieving the hub',
+    });
+});
+});
+
 module.exports = router;
